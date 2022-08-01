877c691e51bd224318dd99e89e9becd593f02c52
Icaro <icaro.ales@hotmail.com>
2022-08-01 17:06:33 -0300

    Adicionado mais um usuario de teste

--- Program.cs
+++ Program.cs
@@ -1,26 +1,9 @@
 ﻿using SenaiWeb.Model;
 
-while(true)
-{
-    Console.WriteLine("O que deseja fazer? 1 - Logar" + "2 - Registrar ou 3 - Sair");
-     string num = Console.ReadLine();
-     if (num == "1")
-     {
-         Console.Clear();
-         Console.WriteLine("Digite seu login e senha, separados por enter.");
-         
-     }
-     else if (num == "2")
-     {
-         Console.Clear();
-     }
-     else if (num == "3")
-     {
-         break;
-     }
-     else
-     {
-         Console.WriteLine("Erro, tente novamente");
-     }
+Usuario usuario = new Usuario();
+usuario.Nome = "Ander Guebs";
+usuario.Senha = "lalala123";
 
-}
+ExemploSenaiContext context = new ExemploSenaiContext();
+context.Add(usuario);
+context.SaveChanges();
