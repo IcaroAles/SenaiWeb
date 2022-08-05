545f8d92703f1875a62c0973c6fdfc7d5467de67
IcaroAles <110416554+IcaroAles@users.noreply.github.com>
2022-08-05 16:59:16 -0300

    Criando a tabela da aula pelo c#

--- Program.cs
+++ Program.cs
@@ -1,6 +1,79 @@
 ﻿using SenaiWeb.Model;
 
-List<int> List = new List<int>();
+List<string> listaNome = new List<string>();
+List<string> listaSobrenome = new List<string>();
+List<object> listaObjeto = new List<object>();
+List<string> listaCaracteres = new List<string>();
+List<string> listaAdjetivos = new List<string>();
+
+listaNome.add = "Marta";
+listaNome.add = "Ronaldo";
+listaNome.add = "Ander";
+listaNome.add = "Maria";
+listaNome.add = "Gustavo";
+listaNome.add = "Pedro";
+listaNome.add = "Guilherme";
+listaNome.add = "Paulo";
+listaNome.add = "Andreia";
+
+listaSobrenome.add = "Silva";
+listaSobrenome.add = "Pereira";
+listaSobrenome.add = "Freitas";
+listaSobrenome.add = "Machado";
+listaSobrenome.add = "Ribeiro";
+listaSobrenome.add = "Souza";
+listaSobrenome.add = "Oliveira";
+listaSobrenome.add = "Adalbert";
+listaSobrenome.add = "Valentin";
+listaSobrenome.add = "Paes";
 
 
-for (int = 0)
+listaObjeto.add = "";
+listaObjeto.add = "";
+listaObjeto.add = "";
+listaObjeto.add = "";
+listaObjeto.add = "";
+listaObjeto.add = "";
+listaObjeto.add = "";
+listaObjeto.add = "";
+listaObjeto.add = "";
+listaObjeto.add = "";
+
+listaCaracteres.add = "A";
+listaCaracteres.add = "B";
+listaCaracteres.add = "C";
+listaCaracteres.add = "D";
+listaCaracteres.add = "E";
+listaCaracteres.add = "F";
+listaCaracteres.add = "G";
+listaCaracteres.add = "H";
+listaCaracteres.add = "I";
+listaCaracteres.add = "J";
+listaCaracteres.add = "K";
+listaCaracteres.add = "L";
+listaCaracteres.add = "M";
+listaCaracteres.add = "N";
+listaCaracteres.add = "O";
+listaCaracteres.add = "P";
+listaCaracteres.add = "Q";
+listaCaracteres.add = "R";
+listaCaracteres.add = "S";
+listaCaracteres.add = "T";
+listaCaracteres.add = "U";
+listaCaracteres.add = "V";
+listaCaracteres.add = "W";
+listaCaracteres.add = "X";
+listaCaracteres.add = "Y";
+listaCaracteres.add = "Z";
+
+
+listaAdjetivos,add = "Intimador";
+listaAdjetivos,add = "Feliz";
+listaAdjetivos,add = "Corajoso";
+listaAdjetivos,add = "Inteligente";
+listaAdjetivos,add = "Bondoso";
+listaAdjetivos,add = "Tranquilo";
+listaAdjetivos,add = "Simpático";
+listaAdjetivos,add = "Rapido";
+listaAdjetivos,add = "Carinhoso";
+listaAdjetivos,add = "Responsável";
